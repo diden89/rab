3d9c6a2b8a7475edc0cb857a0420ea04a9a62431
Dian Kosasih <algazasolution@gmail.com>
2020-10-21 22:13:34 +0700

    update

--- apps/module_frontend/auth/views/auth_view.php
+++ apps/module_frontend/auth/views/auth_view.php
@@ -10,85 +10,101 @@
 ?>
 <!DOCTYPE html>
 <html>
-	<head>
-		<meta charset="utf-8">
-		<meta http-equiv="X-UA-Compatible" content="IE=edge">
-		<meta name="viewport" content="width=device-width, initial-scale=1">
-		<title>Trademark | Log in</title>
-		<link rel="shortcut icon" href="<?php echo base_url('images/favicon.png'); ?>">
-		<link rel="stylesheet" href="<?php echo get_template_url(); ?>plugins/fontawesome-free/css/all.css">
-		<link rel="stylesheet" href="<?php echo base_url('vendors/ionicons/2.0.1/css/ionicons.css'); ?>">
-		<link rel="stylesheet" href="<?php echo get_template_url(); ?>plugins/icheck-bootstrap/icheck-bootstrap.css">
-		<link rel="stylesheet" href="<?php echo get_template_url(); ?>plugins/sweetalert2-theme-bootstrap-4/bootstrap-4.css">
-		<link rel="stylesheet" href="<?php echo get_template_url(); ?>plugins/toastr/toastr.min.css">
-		<link rel="stylesheet" href="<?php echo get_template_url(); ?>dist/css/adminlte.css">
-		<link rel="stylesheet" href="<?php echo base_url('styles/source_sans_pro_300_400_400i_700.css'); ?>">
-		<link rel="stylesheet" href="<?php echo base_url('vendors/jquery_waitme/waitMe.css'); ?>">
-		<link rel="stylesheet" href="<?php echo base_url('styles/styles.css'); ?>">
-	</head>
-	<body class="container-fluid auth-page">
-		<div class="row">
-			<div class="col-sm-6 application-banner">
-				<div class="banner-box">
-					<!-- <img class="banner-img" src="<?php echo base_url('images/big_logo.png'); ?>" alt="logo-merekdagang"> -->
-					<div class="banner-text">
-						<h1><?= NOOBS_TITLE_1 ?></h1>
-						<!-- <small><?= NOOBS_TITLE_2 ?></small> -->
-					</div>
-				</div>
-			</div>
-			<div class="col-sm-6 login-page">
-				<div class="login-box">
-					<div class="card">
-						<div class="card-body login-card-body">
-							<!-- <div class="login-logo">
-								<img src="<?php echo base_url('images/login.png'); ?>">
-							</div> -->
-							<p class="login-box-msg">Please Sign In to Find the Similarity</p>
-							<form id="formLogin" action="#" method="post" autocomplete="off">
-								<div class="input-group mb-3">
-									<input type="text" class="form-control" id="txtUsername" name="txt_username" placeholder="Username" required>
-									<div class="input-group-append">
-										<div class="input-group-text">
-											<span class="fas fa-user"></span>
-										</div>
-									</div>
-								</div>
-								<div class="input-group mb-3">
-									<input type="password" class="form-control" id="txtPassword" name="txt_password" placeholder="Password" required>
-									<div class="input-group-append">
-										<div class="input-group-text">
-											<span class="fas fa-lock"></span>
-										</div>
-									</div>
-								</div>
-								<div class="row">
-									<div class="col-12">
-										<input type="hidden" name="action" value="do_login">
-										<button type="submit" class="btn btn-primary btn-block">Sign In</button>
-									</div>
-								</div>
-							</form>
-						</div>
-					</div>
-				</div>
-			</div>
-		</div>
+  <head>
+    <meta charset="utf-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <title>NOOBS_TITLE_1</title>
+    <meta name="author" content="phpmu.com">
+    <meta content="width=device-width, initial-scale=1, maximum-scale=1, user-scalable=no" name="viewport">
+   	<meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+	<link rel="icon" type="image/png" href="<?php echo base_url('vendors/template/login/'); ?>images/icons/favicon.ico"/>
+	<link rel="stylesheet" type="text/css" href="<?php echo base_url('vendors/template/login/'); ?>vendor/bootstrap/css/bootstrap.min.css">
+  	<link rel="stylesheet" type="text/css" href="<?php echo base_url('vendors/template/login/'); ?>fonts/font-awesome-4.7.0/css/font-awesome.min.css">
+  	<link rel="stylesheet" type="text/css" href="<?php echo base_url('vendors/template/login/'); ?>vendor/animate/animate.css">
+  	<link rel="stylesheet" type="text/css" href="<?php echo base_url('vendors/template/login/'); ?>vendor/css-hamburgers/hamburgers.min.css">
+  	<link rel="stylesheet" type="text/css" href="<?php echo base_url('vendors/template/login/'); ?>vendor/select2/select2.min.css">
+  	<link rel="stylesheet" type="text/css" href="<?php echo base_url('vendors/template/login/'); ?>css/util.css">
+    <link rel="stylesheet" type="text/css" href="<?php echo base_url('vendors/template/login/'); ?>css/main.css">
 
-		<script type="text/javascript">
-			var NOOBS_ENVIRONMENT = '<?php echo ENVIRONMENT; ?>';
-			var NOOBS_LANGUAGE = '<?php echo NOOBS_LANGUAGE; ?>';
-			var NOOBS_TITLE = '<?php echo NOOBS_TITLE; ?>';
-			var BASE_URL = '<?php echo base_url(); ?>';
-		</script>
-		<script src="<?php echo get_template_url(); ?>plugins/jquery/jquery.js"></script>
-		<script src="<?php echo get_template_url(); ?>plugins/bootstrap/js/bootstrap.bundle.js"></script>
-		<script src="<?php echo get_template_url(); ?>plugins/sweetalert2/sweetalert2.js"></script>
-		<script src="<?php echo get_template_url(); ?>plugins/toastr/toastr.js"></script>
-		<script src="<?php echo get_template_url(); ?>dist/js/adminlte.js"></script>
-		<script src="<?php echo base_url('scripts/custom_validity.js'); ?>"></script>
-		<script src="<?php echo base_url('vendors/jquery_waitme/waitMe.js'); ?>"></script>
-		<script src="<?php echo base_url('scripts/scripts.js'); ?>"></script>
-		<script src="<?php echo base_url('scripts/auth/auth.js'); ?>"></script>
-	</body>
-</html>
+  </head>
+  <body>
+    
+    <div class="limiter">
+        <div class="container-login100">
+            <div class="wrap-login100">
+                <div class="login100-pic js-tilt" data-tilt>
+                    <img src="<?php echo base_url('images/'); ?>logo.png" alt="IMG">
+                </div>
+
+                <form class="login100-form validate-form" action="<?php echo site_url('auth/login'); ?>" method="post" autoComplete = "off">
+                    <span class="login100-form-title">
+                        Login
+                    </span>
+
+                    <div class="wrap-input100 validate-input" data-validate = "Valid email is required: ex@abc.xyz">
+                        <input class="input100" type="text" name="txt_username" placeholder="Username">
+                        <span class="focus-input100"></span>
+                        <span class="symbol-input100">
+                            <i class="fa fa-users" aria-hidden="true"></i>
+                        </span>
+                    </div>
+
+                    <div class="wrap-input100 validate-input" data-validate = "Password is required">
+                        <input class="input100" type="password" name="txt_password" placeholder="Password">
+                        <span class="focus-input100"></span>
+                        <span class="symbol-input100">
+                            <i class="fa fa-lock" aria-hidden="true"></i>
+                        </span>
+                    </div>
+                    
+                    <div class="container-login100-form-btn">
+                        <button name='login' type="submit" class="login100-form-btn">
+                            Login
+                        </button>
+                    </div>
+
+                    <div class="text-center p-t-12">
+                        <span class="txt1">
+                            Forgot
+                        </span>
+                        <a class="txt2" href="#">
+                            Username / Password?
+                        </a>
+                    </div>
+
+                    <div class="text-center p-t-136">
+                        <!-- <a class="txt2" href="#">
+                            Create your Account
+                            <i class="fa fa-long-arrow-right m-l-5" aria-hidden="true"></i>
+                        </a> -->
+                    </div>
+                    <input type="hidden" name="action" value="login">
+                </form>
+            </div>
+        </div>
+    </div>
+    
+    
+    <script type="text/javascript">
+		var NOOBS_ENVIRONMENT = '<?php echo ENVIRONMENT; ?>';
+		var NOOBS_LANGUAGE = '<?php echo NOOBS_LANGUAGE; ?>';
+		var NOOBS_TITLE = '<?php echo NOOBS_TITLE; ?>';
+		var BASE_URL = '<?php echo base_url(); ?>';
+	</script>
+    
+	<script src="<?php echo base_url('vendors/template/login/'); ?>vendor/jquery/jquery-3.2.1.min.js"></script>
+	<script src="<?php echo base_url('vendors/template/login/'); ?>vendor/bootstrap/js/popper.js"></script>
+    <script src="<?php echo base_url('vendors/template/login/'); ?>vendor/bootstrap/js/bootstrap.min.js"></script>
+    <script src="<?php echo base_url('vendors/template/login/'); ?>vendor/select2/select2.min.js"></script>
+	<script src="<?php echo base_url('vendors/template/login/'); ?>vendor/tilt/tilt.jquery.min.js"></script>
+    <script >
+        $('.js-tilt').tilt({
+            scale: 1.1
+        })
+    </script>
+	<script src="<?php echo base_url('vendors/template/login/'); ?>js/main.js"></script>
+
+</body>
+</html>   
+
