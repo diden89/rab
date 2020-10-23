389e8a1b38bec5016f885c88c66f50ccbad377f2
Dian Kosasih <algazasoution@gmail.com>
2020-10-23 11:43:26 +0700

    Create master_data.php

---
+++ apps/module_frontend/master_data/master_data.php
@@ -0,0 +1,21 @@
+<?php if ( ! defined('BASEPATH')) exit('No direct script access allowed');
+/*!
+ * @package RAB
+ * @copyright Noobscript
+ * @author Andy1t
+ * @version 1.0
+ * @access Public
+ * @link /ahp_merekdagang_frontend/apps/module_frontend/settings/controllers/Settings.php
+ */
+
+class Master_data extends NOOBS_Controller {
+	function __construct()
+	{
+		parent::__construct();
+	}
+
+	public function index()
+	{
+		$this->show_404();
+	}
+}
